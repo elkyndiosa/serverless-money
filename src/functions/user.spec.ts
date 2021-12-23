0f2304c27e2fa12333178e9f8ab875068d51f035
Elkyn Gustavo Diosa Martinez <elkyn.diosa@linktic.co>
2021-12-23 08:22:04 -0500

    add test for crate user existing

--- src/functions/user.spec.ts
+++ src/functions/user.spec.ts
@@ -4,8 +4,8 @@ import { create } from './user';
 import mock = require('./mock.json');
 
 describe('Validating entity User', () => {
+  mock.body.data.attributes.email = `test-elkyn${Math.random()}@gmail.com`;
   test('Creation of resource', async () => {
-    mock.body.data.attributes.email = `test-elkyn${Math.random()}@gmail.com`;
     const event = eventGenerator(mock);
     const resp = await create(event);
     expect(resp.statusCode).toBe(200);
@@ -15,6 +15,18 @@ describe('Validating entity User', () => {
     expect(body).toEqual({
       message: `User Created Successfully!`,
       user: { ...mock.body.data.attributes, balance: 0, id: expect.any(Number) },
+    });
+  });
+  test('Creation of resource existing', async () => {
+    const event = eventGenerator(mock);
+    const resp = await create(event);
+    expect(resp.statusCode).toBe(400);
+    expect(resp).toBeDefined();
+    expect(isApiGatewayResponse(resp)).toBe(true);
+    const body = JSON.parse(resp.body);
+    expect(body).toEqual({
+      message: `Sorry, User already exists!`,
+      statusCode: 400,
     });
   });
 });
